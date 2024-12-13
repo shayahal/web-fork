ce1cd927dced70f6fe0e0cc65f881e195f843d48
shayahal <shayahal1@gmail.com>
2024-12-13 16:46:23 +0200

    more

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,73 +1,16 @@
 module.exports = {
   siteMetadata: {
-    // Site URL for when it goes live
-    siteUrl: `https://shayahal.github.com/web-fork`,
-    pathPrefix: "/web-fork", 
-    // Your Name
-    name: 'Shay Yahal',
-    // Main Site Title
-    title: `Shay Yahal | Yours`,
-    // Description that goes under your name in main bio
-    description: `Lorem ipsum dolornpm run clean
- sit amet consectetur adipisicing elit, ipsum.`,
-    // Optional: Twitter account handle
-    author: `@shayahal1`,
-    // Optional: Github account URL
-    github: `https://github.com/shayahal`,
-    // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/shay-yahal/`,
-    // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
-    // Optional: List your projects, they must have `name` and `description`. `link` is optional.
-    projects: [
-      {
-        name: 'ShayShu',
-        description:
-          'A zero-config Blah Blah',
-        link: 'Non',
-      },
-      {
-        name: 'This Repo is',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-    ],
-    // Optional: List your recommendations, they must have `name` and `description`. `link` is optional.
-    recommendations: [
-      {
-        name: 'Nothing intersting',
-        description: 'Data Data',
-        link: 'https://github.com/Blahblah',
-      },
-    ],
-    // Optional: List your skills, they must have `name` and `description`.
-    skills: [
-      {
-        name: 'Languages & Frameworks',
-        description:
-          'Python mostly',
-      },
-      {
-        name: 'Databases',
-        description: 'MongoDB, PostreSQL, MySQL',
-      },
-      {
-        name: 'Other',
-        description:
-          'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
-      },
-    ],
+    title: `Shay is writing stuff`,
+    author: {
+      name: `Shay`,
+    },
+    description: `A blog where Shay writes stuff`,
+    siteUrl: `https://your-site-url.com`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -82,8 +25,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590,
-              wrapperStyle: `margin: 0 0 30px;`,
+              maxWidth: 630,
             },
           },
           {
@@ -93,31 +35,8 @@ module.exports = {
             },
           },
           `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
         ],
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-postcss`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: `ADD YOUR TRACKING ID HERE`, // Optional Google Analytics
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `devfolio`,
-        short_name: `devfolio`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`, // This color appears on mobile
-        display: `minimal-ui`,
-        icon: `src/images/icon.png`,
-      },
-    },
   ],
-};
+}
