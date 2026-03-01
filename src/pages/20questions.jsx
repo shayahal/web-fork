19ac96edd13b655ef38fec449d054e4e3aed44f5
shayahal <shayahal1@gmail.com>
2026-03-01 22:45:26 +0200

    feat: create default 20questions page route

---
+++ src/pages/20questions.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import AnalysisTemplate from '../templates/20questions-template';
+import { analysisHTML } from '../data/20questions-html';
+
+const TwentyQuestionsPage = ({ data }) => {
+  return (
+    <AnalysisTemplate
+      language="en"
+      htmlContent={analysisHTML}
+      metadata={data.site.siteMetadata}
+    />
+  );
+};
+
+export default TwentyQuestionsPage;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        name
+        title
+        description
+        author
+      }
+    }
+  }
+`;
